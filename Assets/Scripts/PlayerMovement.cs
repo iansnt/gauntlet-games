68d094aa3d7a6dcc935b88195e4e0736b7a11497
Nick Geofroy <nickgeofroy1998@gmail.com>
2016-07-18 17:44:52 -0400

    changes

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,22 +4,29 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 
     public int movespeed = 5;
+    public int jump = 0;
+    Rigidbody2D rb;
+    Collider2D col;
+    public GameObject other;
+
 	// Use this for initialization
 	void Start ()
     {
-	    
+        rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<Collider2D>();
+        
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate ()
     {
-        if (Input.GetKey("up"))
-            transform.Translate(Vector2.up * Time.deltaTime * movespeed);
-        if (Input.GetKey("down"))
+        if (Input.GetKeyDown("up") && col.IsTouchingLayers())
+            rb.AddForce(Vector2.up * jump);
+        if (Input.GetKey("down") && col.IsTouchingLayers(1))
             transform.Translate(Vector2.down * Time.deltaTime * movespeed);
-        if (Input.GetKey("left"))
+        if (Input.GetKey("left") && col.IsTouchingLayers(1))
             transform.Translate(Vector2.left * Time.deltaTime * movespeed);
-        if (Input.GetKey("right"))
+        if (Input.GetKey("right")&& col.IsTouchingLayers(1))
             transform.Translate(Vector2.right * Time.deltaTime * movespeed);
     }
 }
