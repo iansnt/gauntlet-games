ddfb6b1a1ff3c6f0e6546e535d3aac06232c5c98
Nick Geofroy <nickgeofroy1998@gmail.com>
2016-07-18 16:58:33 -0400

    added movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public int movespeed = 5;
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey("up"))
+            transform.Translate(Vector2.up * Time.deltaTime * movespeed);
+        if (Input.GetKey("down"))
+            transform.Translate(Vector2.down * Time.deltaTime * movespeed);
+        if (Input.GetKey("left"))
+            transform.Translate(Vector2.left * Time.deltaTime * movespeed);
+        if (Input.GetKey("right"))
+            transform.Translate(Vector2.right * Time.deltaTime * movespeed);
+    }
+}
